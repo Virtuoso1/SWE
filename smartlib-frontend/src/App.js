a4e447775505326a2df0008bade4dd5f5c468ffe
Kevoo30 <muokikelvin55@gmail.com>
2025-11-10 18:16:47 +0300

    book details frontend

--- smartlib-frontend/src/App.js
+++ smartlib-frontend/src/App.js
@@ -1,16 +1,23 @@
-import './App.css';
+// App.js
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import BookDetails from "./components/Library/BookDetails";
-import BorrowResponse from "./components/Library/BorrowResponse";
+import BookList from "./components/library/booklist";
+import BookDetails from "./components/library/bookdetails";
+import AddBookForm from "./components/library/addbookform";
+import EditBookForm from "./components/library/editbookform";
+import "./App.css";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/library" element={<BookList />} />
-        <Route path="/book/:id" element={<BookDetails />} />
-        <Route path="/borrow/:id" element={<BorrowResponse />} />
-      </Routes>
+      <div className="App">
+        <Routes>
+          <Route path="/library" element={<BookList />} />
+          <Route path="/library/:id" element={<BookDetails />} />
+          <Route path="/library/add" element={<AddBookForm />} />
+          <Route path="/library/edit/:id" element={<EditBookForm />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
